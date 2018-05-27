3d7204f7e0b4b2648219cc42383c47f5589f9917
顽游 <asdsongguo@163.com>
2018-05-27 23:15:04 +0800

    py+1
     18/5/27

--- python program/6.1.py
+++ python program/6.1.py
@@ -11,5 +11,14 @@ alien_1['points'] = 5
 #######################################
 alien_2 = {'x_position': 0, 'y_position': 25, 'speed':'medium'}
 print("Original x_position:" + str(alien_2['x_position']))
-if alien_0['speed'] == 'slow':
-	
+if alien_2['speed'] == 'slow':
+	x_increment = 1
+elif alien_2['speed'] == 'medium':
+	x_increment = 2
+else:
+	x_increment = 3
+alien_2['x_position'] = alien_2['x_position'] + x_increment
+print("New x_position:" + str(alien_2['x_position']))
+
+del alien_1['points']
+print(alien_1)
